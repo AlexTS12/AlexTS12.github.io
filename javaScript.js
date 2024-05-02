93641a55ac561e68b8917b6f18a93cdcfd016ba8
psyat14 <psyat14@nottingham.ac.uk>
2024-05-02 17:07:39 +0100

    test

--- javaScript.js
+++ javaScript.js
@@ -1,3 +1,25 @@
+import { createClient } from '../@supabase/supabase-js'
+
 const supabaseUrl = 'https://tcobuzjvyosomsgksmlr.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRjb2J1emp2eW9zb21zZ2tzbWxyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMzNTg5NzUsImV4cCI6MjAyODkzNDk3NX0.70kPyp7QAIoEQH0Ji2GkPRFYPfkBpIApriZcKKNmjRg'; // Replace with your Supabase key
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+async function search()
+{
+    let query = supabase.from("persons").select("*");
+
+
+    const { data,error } = await query;
+
+    if (error)
+    {
+        console.log('error', error);
+    }
+    else
+    {
+        console.log('data', data);
+    }
+
+}
+
+search();
