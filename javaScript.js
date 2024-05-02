cab28bada36be7ec2f9e42027bd8133b674a1857
psyat14 <psyat14@nottingham.ac.uk>
2024-05-02 10:04:30 +0100

    5th go database integration

--- javaScript.js
+++ javaScript.js
@@ -1,8 +1,8 @@
-import { createClient } from '@supabase/supabase-js';
-
-// Replace 'YOUR_SUPABASE_URL' and 'YOUR_SUPABASE_KEY' with your actual Supabase URL and API Key
-const supabase = createClient('https://tcobuzjvyosomsgksmlr.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRjb2J1emp2eW9zb21zZ2tzbWxyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMzNTg5NzUsImV4cCI6MjAyODkzNDk3NX0.70kPyp7QAIoEQH0Ji2GkPRFYPfkBpIApriZcKKNmjRg');
 import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://tcobuzjvyosomsgksmlr.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 document.addEventListener('DOMContentLoaded', function () {
     const submitBtn = document.getElementById('submitBtn');
@@ -31,17 +31,17 @@ document.addEventListener('DOMContentLoaded', function () {
             const user = data[0];
             // Display user information in searchResultsDiv
             searchResultsDiv.innerHTML = `
-                <h2>User Information</h2>
-                <p>Name: ${user.name}</p>
-                <p>Address: ${user.address}</p>
-                <p>Date of Birth: ${user.dob}</p>
-                <p>License Number: ${user.licensenumber}</p>
-                <p>Expiry Date: ${user.expirydate}</p>
+                <div class="user-info-box">
+                    <h2>User Information</h2>
+                    <p>Name: ${user.name}</p>
+                    <p>Address: ${user.address}</p>
+                    <p>Date of Birth: ${user.dob}</p>
+                    <p>License Number: ${user.licensenumber}</p>
+                    <p>Expiry Date: ${user.expirydate}</p>
+                </div>
             `;
         } else {
             searchResultsDiv.innerHTML = '<p>No user found.</p>';
         }
     });
 });
-
-
