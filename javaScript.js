e19a324e7e278653bf9b8cb470105b2624a67b4a
psyat14 <psyat14@nottingham.ac.uk>
2024-05-02 12:48:31 +0100

    10th go database integration

--- javaScript.js
+++ javaScript.js
@@ -1,8 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
-
 const supabaseUrl = 'https://tcobuzjvyosomsgksmlr.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRjb2J1emp2eW9zb21zZ2tzbWxyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMzNTg5NzUsImV4cCI6MjAyODkzNDk3NX0.70kPyp7QAIoEQH0Ji2GkPRFYPfkBpIApriZcKKNmjRg'; // Replace with your Supabase key
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 document.addEventListener('DOMContentLoaded', async function () {
     const searchResults = document.getElementById('searchResults');
